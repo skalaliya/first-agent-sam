8fe992baaeda845e69b8cb9b9e44fce85ca584a4
Aymeric Roucher <m-ric@users.noreply.huggingface.co>
2025-02-07 19:28:46 +0000

    Upload tool

---
+++ app.py
@@ -0,0 +1,24 @@
+from smolagents import GradioUI, CodeAgent, HfApiModel
+from scripts.web_search import DuckDuckGoSearchTool
+from scripts.visit_webpage import VisitWebpageTool
+from scripts.final_answer import FinalAnswerTool
+
+model = HfApiModel()
+web_search = DuckDuckGoSearchTool()
+visit_webpage = VisitWebpageTool()
+final_answer = FinalAnswerTool()
+
+agent = CodeAgent(
+    model=model,
+    tools= [web_search, visit_webpage, final_answer],
+    max_steps=6,
+    verbosity_level=1,
+    grammar=None,
+    planning_interval=None,
+    name=None,
+    description=None,
+    authorized_imports=['math', 'time', 're', 'random', 'pandas', 'stat', 'datetime', 'statistics', 'queue', 'itertools', 'collections', 'unicodedata'],
+    prompts_path='./prompts.yaml'
+)
+
+GradioUI(agent).launch()
