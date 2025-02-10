5df72d6ba2487008443b43ba51c70516cbc979bd
Joffrey THOMAS <Jofthomas@users.noreply.huggingface.co>
2025-02-10 08:26:35 +0000

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from tools.final_answer import FinalAnswerTool
 
 from Gradio_UI import GradioUI
 
+# Below is an example of a tool that does nothing. Amaze us with your creativity !
 @tool
 def my_cutom_tool(arg1:str, arg2:int)-> str: #it's import to specify the return type
     #Keep this format for the description / args / args description but feel free to modify the tool
