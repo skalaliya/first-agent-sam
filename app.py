9b5b26a2d70d8b3a19eaebf2dab4f75d2e8a6905
Joffrey THOMAS <Jofthomas@users.noreply.huggingface.co>
2025-02-10 07:51:30 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,35 @@
-
+from smolagents import CodeAgent,DuckDuckGoSearchTool, HfApiModel,load_tool,tool
+import datetime
+import requests
+import pytz
 import yaml
-from smolagents import GradioUI, CodeAgent, HfApiModel
 
-from tools.web_search import DuckDuckGoSearchTool
-from tools.visit_webpage import VisitWebpageTool
-from tools.final_answer import FinalAnswerTool
+from Gradio_UI import GradioUI
+
+@tool
+def my_cutom_tool(arg1:str, arg2:int)-> str: #it's import to specify the return type
+    #Keep this format for the description / args / args description but feel free to modify the tool
+    """A tool that does nothing yet 
+    Args:
+        arg1: the first argument
+        arg2: the second argument
+    """
+    return "What magic will you build ?"
+
+@tool
+def get_current_time_in_timezone(timezone: str) -> str:
+    """A tool that fetches the current local time in a specified timezone.
+    Args:
+        timezone: A string representing a valid timezone (e.g., 'America/New_York').
+    """
+    try:
+        # Create timezone object
+        tz = pytz.timezone(timezone)
+        # Get current time in that timezone
+        local_time = datetime.datetime.now(tz).strftime("%Y-%m-%d %H:%M:%S")
+        return f"The current local time in {timezone} is: {local_time}"
+    except Exception as e:
+        return f"Error fetching time for timezone '{timezone}': {str(e)}"
 
 
 model = HfApiModel(
@@ -14,17 +39,16 @@ model_id='Qwen/Qwen2.5-Coder-32B-Instruct',
 custom_role_conversions=None,
 )
 
-web_search = DuckDuckGoSearchTool()
-visit_webpage = VisitWebpageTool()
-final_answer = FinalAnswerTool()
 
+# Import tool from Hub
+image_generation_tool = load_tool("agents-course/text-to-image", trust_remote_code=True)
 
 with open("prompts.yaml", 'r') as stream:
     prompt_templates = yaml.safe_load(stream)
-
+    
 agent = CodeAgent(
     model=model,
-    tools=[web_search, visit_webpage, final_answer],
+    tools=[image_generation_tool,get_current_time_in_timezone,DuckDuckGoSearchTool()], ## add or remove tools here
     max_steps=6,
     verbosity_level=1,
     grammar=None,
@@ -33,5 +57,6 @@ agent = CodeAgent(
     description=None,
     prompt_templates=prompt_templates
 )
+
 
 GradioUI(agent).launch()
