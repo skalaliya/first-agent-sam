9a8c1129eb74c3e6ace110f9dd6a05f784978712
Joffrey THOMAS <Jofthomas@users.noreply.huggingface.co>
2025-02-11 17:29:13 +0000

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ final_answer = FinalAnswerTool()
 model = HfApiModel(
 max_tokens=2096,
 temperature=0.5,
-model_id='deepseek-ai/DeepSeek-R1-Distill-Qwen-32B',# it is possible that this model may be overloaded
+model_id='https://wxknx1kg971u7k1n.us-east-1.aws.endpoints.huggingface.cloud',# it is possible that this model may be overloaded
 custom_role_conversions=None,
 )
 
